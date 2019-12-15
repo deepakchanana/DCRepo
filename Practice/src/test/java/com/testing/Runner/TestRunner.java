32706da4c124b1918255b228f5c4638a87012077
deepakchanana <deepakdeep1992@gmail.com>
2019-12-15 11:15:05 -0800

    pushing one more

--- Practice/src/test/java/com/testing/Runner/TestRunner.java
+++ Practice/src/test/java/com/testing/Runner/TestRunner.java
@@ -9,5 +9,9 @@ import cucumber.api.junit.Cucumber;
 @CucumberOptions(features="./Feature",glue="deepak",
 format = {"pretty", "html:Reports/cucumber-html-report/Report.html"})
 public class TestRunner {
+	
+	public static void main(String[] args) {
+		System.out.println("Sdfghjmsdafg");
+	}
 
 }
